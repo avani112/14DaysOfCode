f9623228d4ed67400e9755e17e440f3572f490c2
Avani Gokhale <36026630+avani112@users.noreply.github.com>
2019-10-08 10:03:58 +0530

    Create leftrotation.cpp

---
+++ leftrotation.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, d, temp;
+    cin>>n;
+    cin>>d;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin>> a[i];
+    }
+    
+    d=d%n;
+    
+    for(int i=d; i<n; i++){
+        cout<<a[i]<<" ";
+    }
+    for(int i=0; i<d; i++){
+        cout<<a[i]<<" ";
+    }
+}
+
+
